a9536b7f719d5438b8f3694d14975637e3c01944
Enrico Ragusa <enr.ragusa@gmail.com>
2018-01-02 15:07:36 +0100

    Fixed issues with wrapnohup initialization.

--- templwrapnohup.sh
+++ templwrapnohup.sh
@@ -4,9 +4,9 @@ PWD=`pwd`
 ARG1=$@
 PROCID=path_subst
 
-echo "`date`: $$ $PWD $ARG1"  >> $HOME/simulazioni/procID/activenohupID.txt 
+echo "`date`: $$ $PWD $ARG1"  >> $PROCID/activenohupID.txt 
 nohup $ARG1 > "$ARG1.wrapout"
-sed '/'$$'/d' $HOME/simulazioni/procID/activenohupID.txt > $HOME/simulazioni/procID/prov
+sed '/'$$'/d' $PROCID/activenohupID.txt > $PROCID/prov
 mv $PROCID/prov $PROCID/activenohupID.txt
 
 
