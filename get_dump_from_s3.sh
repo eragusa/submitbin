426a86f162e1f645e5af860a0943673509c0597f
benedettaveronesi <benedetta.veronesi@studenti.unimi.it>
2019-03-26 16:50:38 +0100

    Added some files for s3 configuration and usage. Added some changes at the tutorialSim.tex file (information on how to use s3 and Indaco)

---
+++ get_dump_from_s3.sh
@@ -0,0 +1,78 @@
+#!/bin/sh
+#
+# File: get_dump_from_s3.sh
+#
+# Author: Francesco Prelz (Francesco.Prelz@mi.infn.it)
+#
+# Revision history:
+# 12-Feb-2019 Initial version.
+#
+# Description:
+# Stage a file in from S3 using the 's3' (s3lib) command.
+# The needed access credentials for s3lib must be present in the
+# process environment (S3_ACCESS_KEY_ID, S3_SECRET_ACCESS_KEY
+# and, if needed, S3_HOSTNAME).
+# The access bucket can be set either in the environment (S3_BUCKET)
+# or on the command line.
+#
+# TODO: currently key and file names are the same.
+#
+
+
+fst_s3_bucket="$S3_BUCKET"
+
+usage_string="Usage: $0 [-b S3 bucket] inkey1 [,inkey2,...]"
+
+while getopts "b:" arg
+do
+  case "$arg" in
+    b) fst_s3_bucket="$OPTARG" ;;
+    -) break ;;
+    ?) >&2 echo $usage_string
+       exit 1 ;;
+  esac
+done
+
+shift `expr $OPTIND - 1`
+if [ $# -lt 1 ]; then
+  >&2 echo $usage_string
+  exit 1
+fi
+
+if [ "x$fst_s3_bucket" = "x" ]; then
+  >&2 echo "$0: Missing bucket name for S3 I/O"
+  exit 2
+fi
+
+# Locate the S3 executable
+my_s3=`which s3 2> /dev/null`
+
+if [ "x$my_s3" = "x" -o ! -x "$my_s3" ]; then
+  PATH=/bin:/usr/bin:$PATH
+  export PATH
+  my_s3=`which s3 2> /dev/null`
+fi
+
+if [ "x$my_s3" = "x" -o ! -x "$my_s3" ]; then
+  my_s3=`pwd`/s3
+  if [ -e "$my_s3" ]; then
+    chmod +x "$my_s3"
+  fi
+fi
+
+if [ ! -x "$my_s3" ]; then
+  >&2 echo "$0: Cannot find a working copy of the 's3' executable. Exiting"
+  exit 3
+fi
+
+exitcode=0
+
+for in_key in "$@"; do
+  $my_s3 get "$fst_s3_bucket/$in_key" filename="$in_key" 
+  if [ $? -ne 0 ]; then
+    >&2 echo "$0: unable to s3 get $fst_bucket/$in_key."
+    exitcode=`expr $exitcode + 5` 
+  fi
+done
+
+exit $exitcode
