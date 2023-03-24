1005c7d441a54794f2b386681858b04f985549ba
Enrico Ragusa <er198@leicester.ac.uk>
2023-03-24 19:45:40 +0000

    Added script to create scpfile for downloading movies all at ones with correct names

---
+++ scpgen_video.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+ORIGDIR=$PWD
+echo '#!/bin/bash' > moviescript.sh
+echo ' ' >> moviescript.sh
+
+for path in $@; do 
+      cd $path/movie
+      echo ""`scpgen.sh movie.mp4`" "$path".mp4" >> $ORIGDIR/moviescript.sh
+      cd $ORIGDIR
+ done
+
